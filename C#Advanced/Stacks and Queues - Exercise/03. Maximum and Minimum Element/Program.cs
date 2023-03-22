060897f01357299c19b947c268d0b92a1def0cdf
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2023-03-22 23:37:33 +0200

    Add files via upload

---
+++ C#Advanced/Stacks and Queues - Exercise/03. Maximum and Minimum Element/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace ConsoleApp3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            int rows = input[0];
+            int cols = input[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            string snake = Console.ReadLine();
+
+            int index = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                if (row % 2 == 0)
+                {
+
+                    for (int col = 0; col < cols; col++)
+                    {
+                        matrix[row, col] = snake[index];
+                        index++;
+                        if (index >= snake.Length)
+                        {
+                            index = 0;
+                        }
+
+                    }
+                }
+                else
+                {
+                    for (int col = cols - 1; col >= 0; col--)
+                    {
+                        matrix[row, col] = snake[index];
+                        index++;
+
+                        if (index >= snake.Length)
+                        {
+                            index = 0;
+                        }
+                    }
+
+                }
+
+            }
+                PrintMatrix(matrix);
+        }
+        private static void PrintMatrix(char[,] matrix) // Print matrix
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+
+}
