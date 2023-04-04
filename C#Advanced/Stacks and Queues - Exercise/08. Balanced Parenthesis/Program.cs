f2f42f731ccd15325be223e917db2583698f68f0
Valentin Gaidarov <106426081+Tiwua@users.noreply.github.com>
2023-04-04 23:31:24 +0300

    Add files via upload

---
+++ C#Advanced/Stacks and Queues - Exercise/08. Balanced Parenthesis/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace T08Balanced_Parenthesis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> open = new Stack<char>();
+            bool isBalanced = true;
+            foreach (char bracket in input)
+            {
+                if (bracket == '(' || bracket == '{' || bracket == '[')
+                {
+                    open.Push(bracket);
+                }
+                else
+                {
+                    if (!open.Any())
+                    {
+                        isBalanced = false;
+                        Console.WriteLine("NO");
+                        break;
+                    }
+                    else if (bracket == ')' && open.Peek() == '(')
+                    {
+                        open.Pop();
+                    }
+                    else if (bracket == '}' && open.Peek() == '{')
+                    {
+                        open.Pop();
+                    }
+                    else if (bracket == ']' && open.Peek() == '[')
+                    {
+                        open.Pop();
+                    }
+                    else
+                    {
+                        isBalanced = false;
+                        Console.WriteLine("NO");
+                        break;
+                    }
+                }
+            }
+
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
